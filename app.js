95873dec13c8138a1744ee353c4537bc2003680f
komali <95839946+komalioruganti@users.noreply.github.com>
2022-12-06 23:25:47 +0530

    Added various routes to app.js

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const jsdom = require('jsdom');
+const dom = new jsdom.JSDOM("");
+const jquery = require('jquery')(dom.window);
 const app = express();
+const http = require('http');
+const https = require('https');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.get("/",function(req,res){
-var a =6789;
-  res.render('list', {kom: a});
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+var article_search;
+
+app.get("/calendar",function(req,res){
+  res.render('calendar');
+  
+jquery(".day-content").click(function(){
+  jquery('table.month td.day .day-content').css("color","purple");
+})
+  
+})
+
+app.get('/',function(req,res){
+  res.render('landingPage');
+  
+ 
 
 })
 
-app.listen(3000,function(req,res){
+app.get('/chatRoom',function(req,res){
+  res.render('chatRoom');
+});
+
+io.on('connection', (socket) => {
+  console.log("A user is connected");
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+app.get('/searchArticles',function(req,res){
+res.render('search_articles');
+})
+
+app.post('/searchArticles',function(req,res){
+article_search = req.body.searchInput;
+res.redirect("/articles");
+
+})
+app.get('/articles',(req,res)=>{
+  res.render('articles',{article_keyword : article_search })
+})
+
+
+server.listen(3000,function(req,res){
   console.log("The server is running at port 3000");
 })
