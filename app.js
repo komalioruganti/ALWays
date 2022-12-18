086e2c320d93f05d62bb3b5f3755f899bdc33378
komali <95839946+komalioruganti@users.noreply.github.com>
2022-12-18 23:20:46 +0530

    Added Feature: Inserting and Fetching events using database

--- app.js
+++ app.js
@@ -7,34 +7,114 @@ const app = express();
 const http = require('http');
 const https = require('https');
 const server = http.createServer(app);
+const { MongoClient } = require("mongodb");
 const { Server } = require("socket.io");
 const io = new Server(server);
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+const uri =
+  "mongodb+srv://netTheCoder:welcome123@cluster0.ra2eh5p.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 var eventItems = [];
 var article_search;
+var eventsObj = {};
+var resultArr = [];
+var inputEvent,
+  inputDate;
 
-app.get("/calendar",function(req,res){
 
-  res.render('calendar',{newListItem: eventItems});
-  
+app.get("/signUp", function (req, res) {
+  res.render('signUp')
 })
 
-app.post("/calendar",function(req,res){
-var inputEvent = req.body.eventInput;
-eventItems.push(inputEvent);
-res.redirect("/calendar");
-})
-
-app.get('/',function(req,res){
-  res.render('landingPage');
-  
+app.get("/calendar", function (req, res) {
+  console.log(inputDate);
+  res.render('calendar', { newListItem: eventItems, resultArr, eventDate: inputDate });
 
 })
 
-app.get('/chatRoom',function(req,res){
+app.post("/calendar", function (req, res) {
+  inputEvent = req.body.eventInput;
+  console.log(inputEvent);
+  inputDate = req.body.eventDate;
+  eventsObj = {
+    date: inputDate,
+    description: inputEvent
+  }
+  if (inputEvent !== "") {
+    AddEvents().catch(console.dir);
+    eventItems.push(inputEvent);
+  }
+
+
+
+
+  res.redirect("/calendar");
+})
+
+async function AddEvents() {
+  try {
+    const database = client.db('SampleDatabase');
+    const names = database.collection('n');
+    const addedItem = await names.insertOne(eventsObj);
+  }
+
+  finally {
+    //   // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+
+async function main() {
+  const MongoClient = require('mongodb').MongoClient;
+  const uri =
+    'mongodb+srv://netTheCoder:welcome123@cluster0.ra2eh5p.mongodb.net/?retryWrites=true&w=majority'
+
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+
+  // Connect to the client and query
+  await client.connect();
+  findListings(client, 5);
+
+}
+
+main().catch(console.error);
+
+async function findListings(client, resultsLimit) {
+  const cursor = client
+    .db('Calendar')
+    .collection('Events')
+    .find()
+    .limit(resultsLimit);
+
+  const results = await cursor.toArray();
+  if (results.length > 0) {
+    console.log(`Found ${results.length} Events(s):`);
+    results.forEach((result, i) => {
+      resultArr.push(result);
+      console.log(result);
+      date = new Date(result.last_review).toDateString();
+
+
+
+    });
+  }
+}
+
+
+app.get('/', function (req, res) {
+  const date = new Date();
+  res.render('landingPage', { today: date.toDateString() });
+
+
+})
+
+app.get('/chatRoom', function (req, res) {
   res.render('chatRoom');
 });
 
@@ -45,20 +125,20 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('/searchArticles',function(req,res){
-res.render('search_articles');
+app.get('/searchArticles', function (req, res) {
+  res.render('search_articles');
 })
 
-app.post('/searchArticles',function(req,res){
-article_search = req.body.searchInput;
-res.redirect("/articles");
+app.post('/searchArticles', function (req, res) {
+  article_search = req.body.searchInput;
+  res.redirect("/articles");
 
 })
-app.get('/articles',(req,res)=>{
-  res.render('articles',{article_keyword : article_search })
+app.get('/articles', (req, res) => {
+  res.render('articles', { article_keyword: article_search })
 })
 
 
-server.listen(3000,function(req,res){
+server.listen(3000, function (req, res) {
   console.log("The server is running at port 3000");
 })
--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -6,34 +6,36 @@ const currentDate = $(".current-date"),
 let date = new Date(),
     currentYear = date.getFullYear(),
     currentMonth = date.getMonth();
+
 console.log(currentMonth)
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
+const d = currentYear;
+$(".events-date").attr('value', d);
 
 const renderCalendar = () => {
 
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    let firstDayofMonth = new Date(currentYear, currentMonth, 1).getDay(); 
-    let lastDateofMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); 
-    let lastDayofMonth = new Date(currentYear, currentMonth, lastDateofMonth ).getDay()
+    let firstDayofMonth = new Date(currentYear, currentMonth, 1).getDay();
+    let lastDateofMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    let lastDayofMonth = new Date(currentYear, currentMonth, lastDateofMonth).getDay()
     let lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate();
-   
+
     let liTag = "";
 
-    for (let i = firstDayofMonth ; i > 0; i--) {
-        liTag += `<li class="day inactive">${lastDateofLastMonth-i +1}</li>`
-        
+    for (let i = firstDayofMonth; i > 0; i--) {
+        liTag += `<li class="day inactive">${lastDateofLastMonth - i + 1}</li>`
+
     }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
-        let isToday = i ===  date.getDate() && currentMonth === new Date().getMonth() 
-                      && currentYear === new Date().getFullYear() ? "active" :"";
+        let isToday = i === date.getDate() && currentMonth === new Date().getMonth()
+            && currentYear === new Date().getFullYear() ? "active" : "";
         liTag += `<li class="day ${isToday}">${i}</li>`
     }
-   for(let i =lastDayofMonth; i<6 ;i++){
-    liTag += `<li class="day inactive">${i-lastDayofMonth +1}</li>`
-   }
+    for (let i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="day inactive">${i - lastDayofMonth + 1}</li>`
+    }
 
     currentDate.text(`${months[currentMonth]} ${currentYear}`);
     daysTag.html(liTag)
@@ -53,7 +55,7 @@ prevNextIcons.forEach(icon => {
 
             console.log(currentMonth);
             renderCalendar();
-            
+            dateClicked();
         } else {
 
             if (currentMonth === 11) {
@@ -65,18 +67,29 @@ prevNextIcons.forEach(icon => {
             console.log(currentMonth);
 
             renderCalendar();
-            
+            dateClicked();
         }
 
     })
 })
 
-let eventLi ="";
+let dateObj = new Date();
+let today = String(dateObj.getDate()).padStart(2, '0');
+$(".event-date").text(today + " " + months[currentMonth]);
+
+let eventLi = "";
+var eventAddedDay;
+dateClicked();
+function dateClicked() {
     selectedDate = document.querySelectorAll(".day")
-    selectedDate.forEach(day=>{
-        day.addEventListener("click",()=>{
-        var clickedDate = day.textContent;
-        $(".event-date").text(clickedDate + " " + months[currentMonth]);
-        console.log(clickedDate);
+    selectedDate.forEach(day => {
+        day.addEventListener("click", () => {
+            var clickedDate = day.textContent;
+            $(".event-date").text(clickedDate + " " + months[currentMonth]);
+            let clickedMonth = currentMonth + 1;
+            let output = currentYear + '-' + clickedMonth + '-' + clickedDate;
+            console.log(output);
+            $(".events-date").attr('value', output);
         })
     })
+}
